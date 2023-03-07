ecd96747d84ce63a4e82195df29f6548ab7a6863
Feris <s210196@edu.bc.fi>
2023-03-07 12:29:10 +0200

    initcommit

--- Nuolia kaupan/Nuolia kaupan/Program.cs
+++ Nuolia kaupan/Nuolia kaupan/Program.cs
@@ -1,6 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-string karkitilaus = "a";
+﻿string karkitilaus = "a";
 string peratilaus = "b";
 int pituustilaus = 0;
 string haluttupituus;
@@ -8,34 +6,32 @@ string haluttupituus;
 Console.WriteLine("Minkälainen kärki (puu, teräs, timantti) :");
 while (karkitilaus != "puu" || karkitilaus != "teräs" || karkitilaus != "timantti")
 {
-karkitilaus = Console.ReadLine();
-if (karkitilaus == "puu" || karkitilaus == "timantti" || karkitilaus == "teräs")
-{
-break;
-}
+    karkitilaus = Console.ReadLine();
+    if (karkitilaus == "puu" || karkitilaus == "timantti" || karkitilaus == "teräs")
+    {
+        break;
+    }
 }
 Console.WriteLine("Minkälainen perä (lehti, kanansulka, kotkansulka) :");
 while (peratilaus != "lehti" || peratilaus != "kanansulka" || peratilaus != "kotkansulka")
 {
-peratilaus = Console.ReadLine();
-if (peratilaus == "lehti" || peratilaus == "kanansulka" || peratilaus == "kotkansulka")
-{
-break;
-}
+    peratilaus = Console.ReadLine();
+    if (peratilaus == "lehti" || peratilaus == "kanansulka" || peratilaus == "kotkansulka")
+    {
+        break;
+    }
 }
 Console.WriteLine("Nuolen pituus (60-100cm) :");
 while (pituustilaus < 60 || pituustilaus > 100)
 {
-haluttupituus = Console.ReadLine();
-if (int.TryParse(haluttupituus, out pituustilaus) == true && pituustilaus < 100 && pituustilaus > 60)
-{
-break;
-}
+    haluttupituus = Console.ReadLine();
+    if (int.TryParse(haluttupituus, out pituustilaus) == true && pituustilaus < 100 && pituustilaus > 60)
+    {
+        break;
+    }
 }
 Nuoli tilattuNuoli = new Nuoli(karkitilaus, peratilaus, pituustilaus);
-Console.WriteLine("Nuoli maksaa " + tilattuNuoli.PalautaHinta() + " kultaa");
-
-
+Console.WriteLine("Nuoli maksaa " + tilattuNuoli.PalautaHinta + " kultaa");
 
 public class Nuoli
 {
@@ -73,20 +69,24 @@ public class Nuoli
         return;
     }
 
-    public double PalautaHinta()
+    public double PalautaHinta
     {
-        return nuolenhinta;
+        get { return nuolenhinta; }
+        set { nuolenhinta = value; }
     }
-    public string Karki()
+    public string Karki
     {
-        return _karki;
+        get { return _karki; }
+        set { _karki = value; }
     }
-    public string Pera()
+    public string Pera
     {
-        return _pera;
+        get { return _pera; }
+        set { _pera = value; }
     }
-    public double Pituus()
+    public double Pituus
     {
-        return _pituus;
+        get { return _pituus; }
+        set { _pituus = value; }
     }
 }
